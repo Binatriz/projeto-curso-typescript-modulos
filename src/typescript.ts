e0e209f690dc72fa0decdcfdc65bf0515c0d8093
Beatriz Cerqueira <beatrizcerqueira008@gmail.com>
2025-11-04 11:51:37 -0300

    Tipos primitivos

---
+++ src/typescript.ts
@@ -0,0 +1,22 @@
+let valor: number = 3000;
+let nome: string = "";
+let isPago: boolean = false;
+let qualquer: any = "";
+qualquer = 22;
+
+//Arrays
+const lista: number[] = [];
+lista.push(13, 22.5, 22, 89, 1.58);
+
+//Tipos Personalizados (Type Alias)
+type Transacao = {
+    tipoTransicao: string;
+    data: Date;
+    valor: number;
+}
+
+const novaTransacao = {
+    tipoTransacao: "",
+    data: new Date(),
+    valor: 0
+}
